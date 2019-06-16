50839e0f61a9577fb4433a5f416ac84b92a4a3ca
MAshiqRS <49956756+MAshiqRS@users.noreply.github.com>
2019-06-16 02:11:21 -0600

    Create 630A.cpp

---
+++ 630A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,a=1;
+    //int b,c,d,e;
+    scanf("%d",&n);
+/*
+    for(int i=1 ; i<=n ; i++)
+    {
+        a= 5*a ;
+    }
+    b=a%10;
+    c=a/10;
+    d=c%10;
+    e=d*10+b;
+    cout<<e<<endl;
+*/
+    printf("25");
+    return 0;
+
+}
